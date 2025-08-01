ca44c0db22bbd90cc494073e61ef9fe201b92715
Quang Dao <qvd@andrew.cmu.edu>
2025-08-01 07:30:23 -0600

    update import

--- ArkLib.lean
+++ ArkLib.lean
@@ -49,6 +49,7 @@ import ArkLib.Data.FieldTheory.NonBinaryField.Secp256k1
 import ArkLib.Data.Fin.Basic
 import ArkLib.Data.Fin.BigOperators
 import ArkLib.Data.Fin.Lift
+import ArkLib.Data.Fin.Notation
 import ArkLib.Data.Fin.Pad
 import ArkLib.Data.Fin.TakeDrop
 import ArkLib.Data.GroupTheory.PrimeOrder
