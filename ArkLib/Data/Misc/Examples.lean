c974a1cc4ebfe42fe4861b18204e0ae407dbdf37
Alexander Hicks <alexanderlhicks@gmail.com>
2025-09-14 00:58:18 +0100

    feat: add sorry demo

---
+++ ArkLib/Data/Misc/Examples.lean
@@ -0,0 +1,8 @@
+
+/-
+Copyright (c) 2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Your Name
+-/
+
+theorem add_comm_demo (a b : Nat) : a + b = b + a := by sorry
