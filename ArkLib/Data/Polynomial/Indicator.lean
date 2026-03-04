7f61b253cb1c35d54fd45b15193fb8e47de65444
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-03-04 17:44:24 +0000

    Indicator poly properties

--- ArkLib/Data/Polynomial/Indicator.lean
+++ ArkLib/Data/Polynomial/Indicator.lean
@@ -49,7 +49,23 @@ lemma indicator_ne_if_pos_is_nonempty {pos neg : Finset F}
   :
   indicator pos neg ≠ 0 := by 
   rw [Finset.nonempty_iff_ne_empty] at h
-  sorry
+  obtain ⟨x, hx⟩ : ∃ x, x ∈ pos := by
+      -- Since pos is non-empty, we can use the fact that a non-empty finite set has an element.
+      apply Finset.nonempty_iff_ne_empty.mpr h;
+  unfold Polynomial.indicator;
+  intro H; 
+  have := congr_arg ( Polynomial.eval x ) H; 
+  norm_num [ hx, Lagrange.eval_interpolate_at_node ] at this;
+  rw [ Polynomial.eval_finset_sum, Finset.sum_eq_single x ] at this 
+    <;> simp_all +decide [ Lagrange.basis ];
+  · simp_all +decide [ Polynomial.eval_prod, Finset.prod_eq_zero_iff, Lagrange.basisDivisor ];
+    exact this.elim fun a ha => ha.1.1 ( sub_eq_zero.mp ha.2 ▸ rfl );
+  · intro y hy hyx; 
+    rw [ Polynomial.eval_prod ] ; 
+    exact Finset.prod_eq_zero 
+      ( Finset.mem_erase_of_ne_of_mem 
+          ( Ne.symm hyx ) 
+          ( Finset.mem_union_left _ hx ) ) ( by simp +decide [ Lagrange.basisDivisor ] ) ;
 
 lemma indicator_eq_1_on_pos {pos neg : Finset F} {x : F}
   (h_pos : x ∈ pos)
@@ -109,10 +125,67 @@ lemma indicator_degree_lt_of_pos_subset_neg {pos neg : Finset F}
   rw [←Finset.union_eq_right] at h
   rw [h]
 
+lemma indicator_natDegree_lt_of_pos_subset_neg {pos neg : Finset F}
+  (h_nonEmpty : pos.Nonempty)
+  (h : pos ⊆ neg)
+  :
+  (indicator pos neg).natDegree < neg.card := by
+  rw [Polynomial.natDegree_lt_iff_degree_lt 
+        (indicator_ne_if_pos_is_nonempty h_nonEmpty)]
+  exact indicator_degree_lt_of_pos_subset_neg h
 
-    
+lemma indicator_natDegree_lt_of_pos_subset_neg' {pos neg : Finset F}
+  (h_nonEmpty : neg.Nonempty)
+  (h : pos ⊆ neg)
+  :
+  (indicator pos neg).natDegree < neg.card := by
+  by_cases h_pos : pos.Nonempty
+  · exact indicator_natDegree_lt_of_pos_subset_neg h_pos h
+  · rw [Finset.not_nonempty_iff_eq_empty] at h_pos
+    simp [h_pos, h_nonEmpty]
+
+section SingletonIndicator
+
+variable {x : F}
+
+noncomputable def singletonIndicator (x : F) (S : Finset F) : F[X]
+  := indicator {x} S
+
+@[simp]
+lemma singleton_indicator_eq_1 :
+  singletonIndicator x ∅ = 1 := by
+  unfold singletonIndicator
+  rw [indicator_is_1_if_neg_is_empty_and_pos_non_empty (by simp)]
+
+@[simp]
+lemma singleton_indicator_eq_1_on_x {S : Finset F} :
+  (singletonIndicator x S).eval x = 1 := by
+  unfold singletonIndicator
+  rw [indicator_eq_1_on_pos (by simp)]
+
+lemma singleton_indicator_eq_0_on_S_minus_x {S : Finset F} {a : F}
+  (h : a ∈ S \ {x})
+  :
+  (singletonIndicator x S).eval a = 0 := by
+  unfold singletonIndicator
+  rw [indicator_eq_0_on_neg_sub_pos (by simp [h])]
+
+lemma singleton_indicator_degree_lt_of_mem {S : Finset F} 
+  (h : x ∈ S)
+  :
+  (singletonIndicator x S).degree < S.card := by
+  unfold singletonIndicator
+  exact indicator_degree_lt_of_pos_subset_neg (by simp [h])
+
+lemma singleton_indicator_natDegree_lt_of_mem {S : Finset F} 
+  (h : x ∈ S)
+  :
+  (singletonIndicator x S).natDegree < S.card := by
+  unfold singletonIndicator
+  exact indicator_natDegree_lt_of_pos_subset_neg (by simp) (by simp [h])
 
 
+end SingletonIndicator
 
 end
 
