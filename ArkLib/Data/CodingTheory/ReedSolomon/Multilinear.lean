366be58f87056857f39694c0ce6f5eb7ce0819b5
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-05-15 17:34:14 +0100

    WIP

---
+++ ArkLib/Data/CodingTheory/ReedSolomon/Multilinear.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Julian Sutherland, Ilia Vlasov, Aristotle (Harmonic)
+-/
+
+import ArkLib.Data.CodingTheory.ReedSolomon
+import ArkLib.Data.MvPolynomial.Multilinear
+import ArkLib.Data.MvPolynomial.LinearMvExtension
+
+namespace ReedSolomon
+
+open MvPolynomial LinearMvExtension 
+
+variable {F : Type*} [Field F] {ι : Type*} (domain : ι ↪ F)
+
+lemma mem_rs_code_iff_exists_mle
+  {f : ι → F} {deg : ℕ} :
+  f ∈ code domain (2 ^ deg) ↔ 
+    ∃ g : F⦃≤ 1⦄[X (Fin deg)], f = evalOnPoints domain (powAlgHom g.1) := by
+  constructor <;> intro h
+  · sorry
+  · obtain ⟨g, h⟩ := h 
+    apply mem_code_of_polynomial_of_natDegree_lt_of_eval
+      (powAlgHom g.1)
+    · exact Nat.lt_of_le_of_lt powAlgHom_natDegree <| by
+       obtain ⟨g, hg⟩ := g
+       rw [mem_restrictDegree] at hg
+       have htotal : g.totalDegree ≤ 1 := by
+        simp [totalDegree]
+        intro b hcoeff
+
+end ReedSolomon
