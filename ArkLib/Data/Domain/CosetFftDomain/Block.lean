5b845b2b581618926629351009b0d28998a3e7b0
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-07-01 11:59:00 +0100

    Docs

--- ArkLib/Data/Domain/CosetFftDomain/Block.lean
+++ ArkLib/Data/Domain/CosetFftDomain/Block.lean
@@ -7,8 +7,27 @@ Authors: Ilia Vlasov
 import Mathlib.Algebra.Polynomial.Roots
 
 import ArkLib.Data.Domain.CosetFftDomain.Ops
-import ArkLib.Data.Domain.CosetFftDomain.Log
 import ArkLib.Data.Domain.FftDomain.Ops
+
+/-! This module provides a definition of a block of a
+  coset FFT domain (definition 4.16 from [ACFY24]).
+
+## Main definitions
+
+- `block`: A block of a coset FFT domain at a point `x`.
+- `blockIdx`: The indices of the elements of a block of
+  a coset FFT domain at a point `x`.
+
+## Main results
+
+- `card_block_le`: The cardinality bound of a block.
+- `card_blockIdx`: The cardinality of `block` and `blockIdx` coincide.
+
+## References
+
+  * [Arnon, G., Chiesa, A., Fenzi, G., and Yogev, E., *WHIR: Reed–Solomon Proximity Testing
+      with Super-Fast Verification*][ACFY24]
+-/
 
 namespace Domain
 
@@ -23,26 +42,38 @@ variable {ω : D} {k : ℕ} {x y : F}
 
 open Finset Polynomial
 
+/-- The `k`th roots of `x` from the domain `ω`.
+
+  This is the definition 4.16 from [ACFY24]. Note, for simplicity of
+  the definition, we do not require `k` to be a power of two,
+  nor `x` to be from a subdomain. -/
 def block (ω : D) (k : ℕ) (x : F) : Finset F :=
   {y ∈ toFinset ω | y ^ k = x}
 
+/-- An equivalent definition of the membership to a block. -/
 @[simp]
 lemma mem_block :
   y ∈ block ω k x ↔ y ∈ ω ∧ y ^ k = x := by simp [block]
 
+/-- There are no roots of `0` in any domain. -/
 @[simp]
 lemma block_x_0 :
   block ω k 0 = ∅ := by aesop
 
+/-- If `x` is `1` then any element of a domain is its `0`th root.
+  In any other case, `x` does not have roots in the domain. -/
 @[simp]
 lemma block_k_0 :
   block ω 0 x = if x = 1 then toFinset ω else ∅ := by aesop
 
+/-- An alternative definition of `block` in terms of
+  `Polynomial.nthRootsFinset`. -/
 lemma block_eq_nthRootsFinset [NeZero k] :
   block ω k x = nthRootsFinset k x ∩ toFinset ω := by aesop (add unsafe cases Nat)
 
+/-- The cardinality of a block does not exceed its degree. -/
 @[simp]
-lemma block_card_le [NeZero k] :
+lemma card_block_le [NeZero k] :
   (block ω k x).card ≤ k := by
   rw [block_eq_nthRootsFinset]
   exact le_trans (card_le_card inter_subset_left) <| by
@@ -51,10 +82,12 @@ lemma block_card_le [NeZero k] :
       (@Multiset.toFinset_card_le F (Classical.decEq F) _)
       (card_nthRoots _ _)
 
+/-- The set of indices of a block of `ω` at `x` of the degree `k`. -/
 def blockIdx (ω : D) (k : ℕ) (x : F) : Finset ι :=
   {i | ω i ^ k = x}
 
 omit [AddCommGroup ι] [CosetFftDomainClass D ι F] in
+/-- The definition of membership to a `blockIdx`. -/
 lemma mem_blockIdx {i : ι} :
   i ∈ blockIdx ω k x ↔ ω i ^ k = x := by simp [blockIdx]
 
@@ -66,6 +99,7 @@ lemma mem_blockIdx_self {i : ι} :
 lemma mem_blockIdx_iff_mem_block {i : ι} :
   i ∈ blockIdx ω k x ↔ ω i ∈ block ω k x := by simp [blockIdx]
 
+/-- `blockIdx` is the preimage of `block`. -/
 lemma blockIdx_eq_preimage_block :
   blockIdx ω k x =
     preimage
@@ -73,6 +107,7 @@ lemma blockIdx_eq_preimage_block :
       (fun _ _ _ _ h ↦ CosetFftDomainClass.injective _ h) := by
   aesop (add simp [mem_blockIdx_iff_mem_block])
 
+/-- The cardinality of `blockIdx` is that of `block`. -/
 @[simp]
 lemma card_blockIdx :
   (blockIdx ω k x).card = (block ω k x).card := by
