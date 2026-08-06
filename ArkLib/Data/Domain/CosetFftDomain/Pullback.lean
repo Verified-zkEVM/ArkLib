cb489ce98aee2876cfe42c2625306da24bb59596
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-08-06 09:41:30 +0100

    Sorry Aristotle :(

--- ArkLib/Data/Domain/CosetFftDomain/Pullback.lean
+++ ArkLib/Data/Domain/CosetFftDomain/Pullback.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024-2026 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: František Silváši, Ilia Vlasov, Aristotle (Harmonic)
+Authors: František Silváši, Ilia Vlasov
 -/
 
 import ArkLib.Data.Domain.CosetFftDomain.Subdomain
