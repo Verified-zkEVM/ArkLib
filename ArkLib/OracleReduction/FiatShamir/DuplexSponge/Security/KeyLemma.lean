0ed20b63abf506297edd84b3efcfa6b4cea0fb66
Chung Thai Nguyen <chung.thai.ng@gmail.com>
2026-09-03 17:25:24 +0700

    docs(dsfs): credit Michele Orrù in Section 5 files

--- ArkLib/OracleReduction/FiatShamir/DuplexSponge/Security/KeyLemma.lean
+++ ArkLib/OracleReduction/FiatShamir/DuplexSponge/Security/KeyLemma.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024-2026 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Quang Dao, Devon Tuma, Chung Thai Nguyen
+Authors: Quang Dao, Devon Tuma, Chung Thai Nguyen, Michele Orrù
 -/
 
 import ArkLib.OracleReduction.FiatShamir.Basic
