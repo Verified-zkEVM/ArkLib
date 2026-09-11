601861f1803ee93fc684522cd437675d0e968f6a
Quang Dao <qvd@andrew.cmu.edu>
2026-09-10 19:08:36 -0700

    chore: expose D5 leading-coefficient descent

--- ArkLib.lean
+++ ArkLib.lean
@@ -871,6 +871,8 @@ public import ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.FiniteRepresenta
 public import
 ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.FirstOrderNormDecoder.D5.CoefficientSplit
 public import
+ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.FirstOrderNormDecoder.D5.LeadingCoefficient
+public import
 ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.FirstOrderNormDecoder.FiberPreprocess
 public import ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.NormSieve.MultiplicitySupport
 public import ArkLib.Data.CodingTheory.ReedSolomon.ListDecoding.NormSieve.ProductFilter
