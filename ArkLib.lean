79cdd65fa971423608e0a4003b5a72ac22990528
Quang Dao <qvd@andrew.cmu.edu>
2026-09-22 16:37:02 -0400

    chore: regenerate ArkLib.lean

--- ArkLib.lean
+++ ArkLib.lean
@@ -337,7 +337,6 @@ public import ArkLib.Data.Polynomial.Bivariate
 public import ArkLib.Data.Polynomial.BivariateFactorDegrees
 public import ArkLib.Data.Polynomial.ClassicalWronskian
 public import ArkLib.Data.Polynomial.Differential.BaseChange
-public import ArkLib.Data.Polynomial.Differential.BaseChange
 public import ArkLib.Data.Polynomial.Differential.Basic
 public import ArkLib.Data.Polynomial.Differential.ChainWitness
 public import ArkLib.Data.Polynomial.Differential.DerivativeDescent
